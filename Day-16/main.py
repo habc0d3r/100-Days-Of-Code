425725be3e3738a6c94b8742130ceb045f1f93d8
habc0d3r <habcoder@gmail.com>
2021-09-27 10:26:54 +0530

    add new code files

---
+++ Day-16/main.py
@@ -0,0 +1,28 @@
+# import another_module
+# print(another_module.another_variable)
+
+# import turtle
+# timmy = turtle.Turtle()
+# timmy is an object, turtle is the module
+# and Turtle() is the class. We are tapping into the Turtle class
+# which is declared in the turtle module and constructing an
+# object called timmy using () with class
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle"), timmy.color('DarkOrchid1')
+# for i in range(3):
+#     timmy.forward(100), timmy.left(120)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column('Sl. no.', [1, 2, 3])
+table.add_column('Pokemon Name', ['Pikachu', 'Squirtle', 'Charmander'])
+table.add_column('Type', ['Electric', 'Water', 'Fire'])
+# print(table.align)
+table.align = 'l'
+
+print(table)
